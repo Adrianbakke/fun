9ae2b12e15dd77907dcb20e3b6666a14d7e0a661
Adrian Bakke <adrian.bakke@yahoo.no>
2020-02-19 20:00:06 +0100

    seems to be working

--- nn.py
+++ nn.py
@@ -7,7 +7,7 @@
 # T (0,1)     F (1,1) 
 # -
 # -
-# F (0,0) - - T (0, 1)
+# F (0,0) - - T (0,1)
 #
 
 import numpy as np
@@ -36,38 +36,41 @@ class NN:
     else: x[x<=0] = 0
     return x
 
-  def _sigmoid(self, x, deriv=False):
+  def _sigmoid(self, x):
     if not isinstance(x,np.ndarray): x = np.array(x)
-    x = 1/(1+np.exp(-x))
-    if deriv: x = x*(1-x)
-    return x
+    return 1/(1+np.exp(-x))
+
+  def _deriv_sigmoid(self, sigm):
+    return sigm*(1-sigm)
 
   def _weight_row(self, n):
     univec = np.random.uniform(0,1,n)
     return (univec/sum(univec))-(1/n)
 
   def forward(self):
-    fst_w = np.array([self._weight_row(self.X.shape[0]) for _ in range(self.X.shape[1])])     
-    snd_w = self._weight_row(self.X.shape[0]).reshape((1,4)).T
-    for c in range(1000):
-      fst = self._sigmoid(self.X@fst_w)
-      snd = self._sigmoid(fst@snd_w)
-      fst_w,snd_w = self._backwards(fst_w, snd_w, snd)
-      if c%100 == 0: print(self._calc_loss(snd))
-
-  def _backwards(self, w1, w2, op, n=1e-3):
-    d1 = self._deriv_loss(op) * self._sigmoid(w2, deriv=True)
-    d2 = d1 * self._sigmoid(w1, deriv=True)
-    neww1 = w1 - n * d1
+    #X::4x2 w1::4x2 w2::1x4 a1::4x4 a2::1x4
+    w1 = np.array([self._weight_row(self.X.shape[1]) for _ in range(self.X.shape[0])])     
+    w2 = self._weight_row(self.X.shape[0]).reshape((1,4))
+    for c in range(10000):
+      a1 = self._sigmoid(w1 @ self.X.T)
+      a2 = self._sigmoid(a1 @ w2.T) # this is really a2.T :: 4x1
+      w1,w2 = self._backwards(w1, w2, a1, a2)
+      if c%1000 == 0:
+         print(self._calc_loss(a2))
+    print(a2)
+    
+  def _backwards(self, w1, w2, a1, a2, n=1e-1):
+    d2 = (a1.T @ (self._deriv_loss(a2) * self._deriv_sigmoid(a2))).T
+    d1 = ((self._deriv_loss(a2) * self._deriv_sigmoid(a2)) @ w2) @ self.X
     neww2 = w2 - n * d2
+    neww1 = w1 - n * d1
     return neww1,neww2
 
-  def _calc_loss(self, res):
-    return np.mean(np.sqrt((res-self.y)**2))
+  def _calc_loss(self, o):
+    return np.mean(np.sqrt((self.y-o)**2))
 
   def _deriv_loss(self, x):
-    return 2*(x-self.y) 
- 
+    return 2*(x-self.y)
+
 a = NN(X,y)
 a.forward()
-
